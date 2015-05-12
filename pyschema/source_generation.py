866d2830c2963f87f3ccb51068a4dabc369b8a70
Anders Nyman <anders@hyresbevakning.se>
2015-05-12 22:26:30 +0200

    Adding docstring

--- pyschema/source_generation.py
+++ pyschema/source_generation.py
@@ -124,6 +124,17 @@ class PackageBuilder(object):
 
 
 def to_python_package(classes, target_folder, parent_package=None, indent=DEFAULT_INDENT):
+    '''
+    This function can be used to build a python package representation of pyschema classes.
+    One module is created per namespace in a package matching the namespace hierarchy.
+
+    Args:
+        classes: A collection of classes to build the package from
+        target_folder: Root folder of the package
+        parent_package: Prepended on all import statements in order to support absolute imports.
+            parent_package is not used when building the package file structure
+        indent: Indent level. Defaults to 4 spaces
+    '''
     PackageBuilder(target_folder, parent_package, indent).from_classes_with_refs(classes)
 
 

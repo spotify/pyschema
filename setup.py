3018c1079907e1b886e97ae508e6aae8fa236d37
Anders Nyman <anders@hyresbevakning.se>
2015-10-12 14:24:05 +0200

    Bump version to 2.4.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name="pyschema",
-    version="2.3.0",
+    version="2.4.0",
     description="Schema definition and serialisation library",
     author="Elias Freider",
     author_email="freider@spotify.com",
